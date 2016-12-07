f4c8dd024764d9c92e2f63f4f97d71693ead0878
Mark Swift <mark@useprofile.com>
2016-12-07 09:15:22 +0000

    Format improvements

--- api.js
+++ api.js
@@ -1,11 +1,9 @@
-module.exports = [
-    {
-        description: 'Restart application',
-        method: 'POST',
-        path: '/restart/',
-        fn: function() {     
-        	Homey.log('[API] Restart application')
-            Homey.app.appRestart()
-        }
+module.exports = [{
+    description: 'Restart application',
+    method: 'POST',
+    path: '/restart/',
+    fn: function() {
+        Homey.log('[API] Restart application')
+        Homey.app.appRestart()
     }
-]
+}]
